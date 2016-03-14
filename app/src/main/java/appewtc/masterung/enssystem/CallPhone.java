07fa02da2a89d61fdb2575e0e9084653006c6161
TaweevatKao <kamiakuma2013@gmail.com>
2016-03-14 16:17:59 +0700

    Data Phone  Finish

--- app/src/main/java/appewtc/masterung/enssystem/CallPhone.java
+++ app/src/main/java/appewtc/masterung/enssystem/CallPhone.java
@@ -2,12 +2,39 @@ package appewtc.masterung.enssystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class CallPhone extends AppCompatActivity {
+
+    //Explicit
+    private LinearLayout topLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_phone);
+
+        //bindwidget
+        bindwidget();
+
+        //showchoose
+        showChoose();
+
+    }//main method
+
+    private void showChoose() {
+
+        int intBackGround = getIntent().getIntExtra("Icon", R.drawable.catagory1);
+        topLinearLayout.setBackgroundResource(intBackGround);
+
     }
-}
+
+    private void bindwidget() {
+        topLinearLayout = (LinearLayout) findViewById(R.id.linTopCallPhone);
+
+
+    }
+
+
+} // main class
