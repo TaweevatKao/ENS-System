ede863b6da9576869591af103f46b65ba0af4e47
TaweevatKao <kamiakuma2013@gmail.com>
2016-03-14 10:33:49 +0700

    Start UI News Finish

--- app/src/main/java/appewtc/masterung/enssystem/NewsLsitView.java
+++ app/src/main/java/appewtc/masterung/enssystem/NewsLsitView.java
@@ -2,6 +2,7 @@ package appewtc.masterung.enssystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewsLsitView extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class NewsLsitView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_lsit_view);
+    } // main method
+
+    public void clickbackNewsList(View view) {
+        finish();
     }
-}
+
+}  // main class
